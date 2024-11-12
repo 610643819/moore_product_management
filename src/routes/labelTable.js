fb43e90969785f24674cf2d6394d8f89b034f854
Brown <give610643819@gmail.com>
2024-11-12 19:33:06 +0800

    labelTable push借口用post

--- src/routes/labelTable.js
+++ src/routes/labelTable.js
@@ -4,7 +4,7 @@ const {pushLabelItem} = require('../controllers/labelTable.js')
 const handleLoginRouter = (req, res) => {
     // 处理路由的逻辑
     // const method = req.method;  // 请求方式
-    if ((req.method === 'GET' || req.method === 'OPTIONS') && req.path === '/api/labelTable/push') {
+    if ((req.method === 'GET') && req.path === '/api/labelTable/push') {
         // return login(req.body).then((data) => {
         //     return new SuccessModel(data)
         // })
@@ -13,21 +13,21 @@ const handleLoginRouter = (req, res) => {
         })
     }
     // 處理異步請求
-    // if (req.method === 'get' && req.path === '/api/login/login') {
-    //     console.log(321)
-    //     // const dataObj = login()
-    //     // // 业务逻辑在controllers模块
-    //     // return login(req.body).then((result) => {
-    //     //     // console.log('result=========>', result);
-    //     //     return new SuccessModel(result)
-    //     // })
-    //     //
-    //     return login(req.body).then((data) => {
-    //         return new SuccessModel(data)
-    //     })
-    //
-    //
-    // }
+    if ((req.method === 'POST' || req.method === 'OPTIONS') && req.path === '/api/labelTable/push') {
+        console.log(321)
+        // const dataObj = login()
+        // // 业务逻辑在controllers模块
+        // return login(req.body).then((result) => {
+        //     // console.log('result=========>', result);
+        //     return new SuccessModel(result)
+        // })
+        //
+        return login(req.body).then((data) => {
+            return new SuccessModel(data)
+        })
+
+
+    }
 }
 
 module.exports = handleLoginRouter
